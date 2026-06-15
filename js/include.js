d08caf14ed55353e3fc6c151227a4d6a4a52855a
murphym215 <murphym215@gmail.com>
2026-06-15 16:48:32 -0400

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,6 +1,10 @@
-fetch('/partials/nav.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('nav-placeholder').innerHTML = data;
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('/partials/nav.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('nav-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading nav:', error));
+});
+
 
